3aff2d8844150b9d9b4765c22edf1c9c2fd257ef
Heinz Beck <hb@ideensiche.de>
2025-03-29 19:19:38 +0100

    change picture position

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <img src="./assets/dummy-1820x1024-Glass.jpg" />
+      <img src="/dummy-1820x1024-Glass.jpg" />
       home page
     </div>
   );
